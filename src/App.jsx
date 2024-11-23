15bfb24983a0d48d73d66a0f6e343fdf5045bcc4
deepakkroxford <93421333+deepakkroxford@users.noreply.github.com>
2024-11-24 03:26:00 +0530

    footer section

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Contact from './pages/Contact'
 import Profile from './pages/Profile'
 import Appointment from './pages/Appointment'
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 
 function App() {
@@ -28,7 +29,7 @@ function App() {
         <Route path='/profile' element={<Profile />} />
         <Route path='/appointment' element={<Appointment />} />
       </Routes>
-
+      <Footer/>
     </div>
   )
 }
