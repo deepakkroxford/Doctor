04a4b1b92b7fefb3bc1fa9c1772f3dee77dd2c46
deepakkroxford <93421333+deepakkroxford@users.noreply.github.com>
2024-11-29 21:40:48 +0530

    setup backend

---
+++ backend/routes/adminRoute.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { addDoctor,loginAdmin } from '../controllers/adminController.js';
+import upload from '../middleware/multer.js';
+import authAdmin from '../middleware/authAdmin.js'
+
+const adminRoute = express.Router();
+adminRoute.post('/add-doctor',authAdmin,upload.single('image'),addDoctor);
+adminRoute.post('/login',loginAdmin);
+
+export default adminRoute;
